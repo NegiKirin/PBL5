eff3c9e71b1ab19cea391e310fc35dd6a0dcb571
khanhhan20033 <106676797+khanhhan20033@users.noreply.github.com>
2024-05-28 15:55:20 +0700

    update

--- Server/DAO/UserDAO.py
+++ Server/DAO/UserDAO.py
@@ -16,26 +16,27 @@ class UserDAO:
             user = []
             # user = User(item[0], item[1], item[2])
             for item in result:
-                user = User(item[0], item[1], item[2],item[3],item[4],item[5],item[6],item[7],item[8],item[9])
+                user = User(item[0], item[1], item[2], item[3], item[4], item[5], item[6], item[7], item[8], item[9])
 
             return user
         except Exception as e:
             print(e)
             return {}
 
-    def insertNewUser(self,dic):
+    def insertNewUser(self, dic):
         try:
             sql_check_username = 'SELECT * FROM user WHERE username = %s'
             self.myCursor.execute(sql_check_username, [dic['username']])
             result = self.myCursor.fetchall()
             if result == []:
                 sql = 'INSERT INTO user (username, password,phone,id_role) VALUES ( %s, %s, %s,1)'
-                self.myCursor.execute(sql, [dic['username'],dic['password'], dic['phone']])
+                self.myCursor.execute(sql, [dic['username'], dic['password'], dic['phone']])
                 self.connect.commit()
                 get_Infor = 'SELECT * FROM user WHERE username = %s'
                 self.myCursor.execute(get_Infor, [dic['username']])
                 result = self.myCursor.fetchall()
-                user = User(result[0][0],result[0][1],result[0][2],result[0][3],result[0][4],result[0][5],result[0][6],result[0][7],result[0][8],result[0][9])
+                user = User(result[0][0], result[0][1], result[0][2], result[0][3], result[0][4], result[0][5],
+                            result[0][6], result[0][7], result[0][8], result[0][9])
                 print(user)
                 return user
             else:
@@ -44,20 +45,23 @@ class UserDAO:
         except Exception as e:
             print(e)
 
-    def UpdateUser(self,dic):
+    def UpdateUser(self, dic):
         try:
             sql = 'UPDATE user SET  lastname = %s ,firstname = %s ,email = %s, gender = %s , phone = %s WHERE username = %s'
-            self.myCursor.execute(sql, [dic['lastname'],dic['firstname'],dic['email'],dic['gender'],dic['phone'],dic['username']])
+            self.myCursor.execute(sql, [dic['lastname'], dic['firstname'], dic['email'], dic['gender'], dic['phone'],
+                                        dic['username']])
             self.connect.commit()
+
             get_Infor = 'SELECT * FROM user WHERE username = %s'
             self.myCursor.execute(get_Infor, [dic['username']])
             result = self.myCursor.fetchall()
-            user = User(result[0][0], result[0][1], result[0][2], result[0][3], result[0][4], result[0][5],result[0][6], result[0][7], result[0][8], result[0][9])
+            user = User(result[0][0], result[0][1], result[0][2], result[0][3], result[0][4], result[0][5],
+                        result[0][6], result[0][7], result[0][8], result[0][9])
             return user
         except Exception as e:
             print(e)
 
-    def Change_Password(self,dic):
+    def Change_Password(self, dic):
         try:
             sql = 'UPDATE user SET  password = %s WHERE username = %s'
             self.myCursor.execute(sql, [dic['password'], dic['username']])
